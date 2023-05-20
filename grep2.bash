487296e7f57df847276b9fed024460f63ac05a2a
angelDariaux <angeldariaux@gmail.com>
2023-05-19 21:19:27 -0600

    finished grep 2 insensitve search case

--- grep2.bash
+++ grep2.bash
@@ -1,14 +1,12 @@
 #!/bin/bash 
 
-# You are given a text file that will be piped into your command through STDIN.
-# Use grep to display all the lines that contain the word the in them.
-# The search should be sensitive to case. Display only those lines of the input file that contain the word 'the'.
+# You are given a text file that will be piped into your command through STDIN. Use grep to display all those lines that contain the word the in them.
+# The search should NOT be sensitive to case.
+# Display only those lines of the input file that contain the word 'the'.
 
 
+# So, when you use the flags -wi together in grep, 
+# it will search for the specified pattern as a whole word (ignoring partial matches) 
+# and perform a case-insensitive search. 
 
-# The -w option in grep stands for "word match". 
-# When this option is used, grep searches for the specified pattern as a whole word,
-# rather than as part of a larger word.
-
-grep -w 'the'
-
+grep -wi 'the'
